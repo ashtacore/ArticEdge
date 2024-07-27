3050f5302b8147dbd1a4fcd8d101ef2c6ef07fe8
Joshua Runyan <runyan@ashtacore.com>
2024-07-27 15:48:57 -0700

    Fix Claude's halucinations

--- scripts/build_project.tcl
+++ scripts/build_project.tcl
@@ -1,5 +1,10 @@
+# Execute from this script's location
+set script_path [file normalize [info script]]
+set script_dir [file dirname $script_path]
+cd $script_dir
+
 # Create project
-create_project arctic_edge_core ./vivado_project -part xc7a100tcsg324-1
+create_project arctic_edge_core ../vivado_project -part xc7a100tcsg324-1
 
 # Set project properties
 set_property board_part digilentinc.com:nexys-a7-100t:part0:1.0 [current_project]
@@ -8,13 +13,13 @@ set_property board_part digilentinc.com:nexys-a7-100t:part0:1.0 [current_project
 set_property target_language SystemVerilog [current_project]
 
 # Add source files recursively
-add_files -fileset sources_1 [glob -nocomplain -directory ./src/rtl -type f -recursive *.sv]
+add_files -fileset sources_1 [glob -nocomplain -directory ../src/rtl -types f **/*.sv]
 
 # Add testbench files
-add_files -fileset sim_1 [glob ./src/tb/*.sv]
+add_files -fileset sim_1 [glob ../src/tb/*.sv]
 
 # Add constraints file
-add_files -fileset constrs_1 ./src/constraints/Nexys_A7_100T.xdc
+add_files -fileset constrs_1 ../src/constraints/Nexys_A7_100T.xdc
 
 # Set top module
 set_property top cpu_core [current_fileset]
